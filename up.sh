ea8434015c1fd953ec924e244de36411b66399da
alexgor <o.gorbatiuk@internetdevels.com>
2016-09-16 18:04:08 +0300

    Rebuilded installation process to use make files.

--- up.sh
+++ up.sh
@@ -1,5 +1,9 @@
 #/bin/sh
 
+sudo rm -rf htdocs
+mkdir -p htdocs/profiles
+cp -r project htdocs/profiles
+
 CONTAINER_ID=$(docker ps -a -q --filter="name=chmez_github")
 
 if [ $CONTAINER_ID ]
@@ -10,17 +14,21 @@ if [ $CONTAINER_ID ]
 fi
 
 docker build -t chmez/ubuntu:latest .
-docker run --net=host -d -i -t --name chmez_github chmez/ubuntu:latest
+docker run --net=host -d -i -t -v $(pwd)/htdocs:/var/www/html --name chmez_github chmez/ubuntu:latest
 
 docker exec -it chmez_github service apache2 start
 docker exec -it chmez_github service mysql start
 
-docker cp project chmez_github:/var/www/html/profiles
-docker exec -it chmez_github mkdir profiles/project/themes
-docker exec -it chmez_github drush dl bootstrap --destination=profiles/project/themes
+cd htdocs
+cp ../*.make.yml .
 
+docker exec -it chmez_github drush make profile.make.yml --prepare-install --overwrite -y
+
+rm *.make.yml
+cd ..
+
+docker exec -it chmez_github chown -R www-data:www-data sites/default
 docker exec -it chmez_github drush si project --db-url=mysql://root:root@localhost:3306/drupal --account-name=root --account-pass=root --account-mail=admin@example.com --locale=uk --site-name="My Project" --site-mail=admin@example.com -y
 
 docker exec -it chmez_github chown -R www-data:www-data sites/default/files
-
 docker exec -it chmez_github drush cr
